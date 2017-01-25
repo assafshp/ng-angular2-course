cd5276c51ab52423fd1ed3aab1c62c4297bdf7f9
assafhp <assafshp@zahav.net.il>
2017-01-25 16:29:28 +0200

    formModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,36 +36,136 @@
 
 
 import {Component, ViewEncapsulation} from "@angular/core";
+import {FormGroup, FormControl, FormArray, FormBuilder} from "@angular/forms";
+import {register} from "ts-node/dist";
 
 @Component({
   // encapsulation: ViewEncapsulation.Native,
   selector: 'app-root',
   template: `
 
-      <todo-app></todo-app>
-      <todo-app></todo-app>
-      <todo-app></todo-app>
+      <!--<todo-app></todo-app>-->
+      <!--<todo-app></todo-app>-->
+      <!--<todo-app></todo-app>-->
+      <!---->
+      <!--<h1>My App</h1>-->
+    <!---->
+      <!--<input type="checkbox" [(ngModel)]="flag"/>-->
+      <!---->
+      <!--<h2 *appIf="flag" appFocus>Hello Template</h2>-->
+      <!---->
+      <!--<input *appIf="flag" appFocus=""/>-->
       
-      <h1>My App</h1>
-    
-      <input type="checkbox" [(ngModel)]="flag"/>
+      <h1>Reactive Forms Modules</h1>
       
-      <h2 *appIf="flag" appFocus>Hello Template</h2>
+      <form [formGroup]="userForm">
+      <fieldset>
+        <input type="text" formControlName="username" placeholder="username"/>
+        <input type="password" formControlName="password" placeholder="password"/>
+      </fieldset>
       
-      <input *appIf="flag" appFocus=""/>
+      <fieldset formGroupName="address">
+        <legend>Address:</legend>
+        <input type="text" formControlName="city" placeholder="city"/>
+        <input type="text" formControlName="street"  placeholder="street"/>
+      </fieldset>
+      
+      <fieldset formArrayName="phones">
+        <legend>Phones</legend>
+        <input type="text"
+               *ngFor="let phone of userForm.get('phones').controls; let i=index"
+               [formControlName]="i"
+               placeholder="phone"/>
+        <button type="button" (click)="addPhone()">+</button>
+      </fieldset>
+      
+      </form>
+      
       
   `,
   styles: ['h1 {text-align:center;color:red}']
 })
 
 
-export class AppComponent{
+export class AppComponent {
 
-  private title: string;
+  // private title: string;
+  //
+  // constructor() {
+  //   this.title = "Hello Angular";
+  // }
 
-  constructor() {
-    this.title = "Hello Angular";
+  private userForm  : FormGroup;
+  // private address   : FormGroup;
+  // private phones    : FormArray;
 
 
+  constructor(builder: FormBuilder) {
+
+    this.userForm = builder.group({
+      username: 'Assaf',
+      password: '12345',
+      address: builder.group({
+        city: 'Eilat',
+        street: 'ddd'
+      }),
+      phones: builder.array([ new FormControl('1212121')])
+    });
+
+    // console.log()
+
+    this.register();
   }
+
+  public register(){
+    console.log((this.userForm.value));
+  }
+
+  public addPhone(){
+    const phonesArr = <FormArray>this.userForm.get('phones');
+    phonesArr.push(new FormControl());
+  }
+
+// private userForm  : FormGroup;
+  // private usertName : FormControl;
+  // private password  : FormControl;
+  //
+  // private address   : FormGroup;
+  // private city      : FormControl;
+  // private street    : FormControl;
+  //
+  // private phones    : FormArray;
+  //
+  //
+  // constructor() {
+  //   this.usertName = new FormControl('Assaf');
+  //   this.password = new FormControl('1234');
+  //
+  //   this.city = new FormControl('Eilat');
+  //   this.street = new FormControl('Dizingof');
+  //
+  //   this.phones = new FormArray([new FormControl('11111111')]);
+  //
+  //   this.address = new FormGroup({
+  //     city: this.city,
+  //     street: this.street
+  //   });
+  //
+  //
+  //   this.userForm = new FormGroup({
+  //     username: this.usertName,
+  //     password: this.password,
+  //     address: this.address,
+  //     phones: this.phones
+  //   })
+  //   console.log(this.userForm.value);
+  //   console.log(this.address.value);
+  //
+  // }
+
+  // send(){
+  //   if (this.usertName.value !== 'sdfds'){
+  //
+  //   }
+  // }
 }
